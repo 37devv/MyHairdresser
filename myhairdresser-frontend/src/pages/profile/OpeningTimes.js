be606f10051c92de2e070758c22073472f2fc8cc
iribzuehlke <irdin.ibisevic@gmail.com>
2023-09-27 10:47:33 +0200

    Undefined opening times error works now

--- myhairdresser-frontend/src/pages/profile/OpeningTimes.js
+++ myhairdresser-frontend/src/pages/profile/OpeningTimes.js
@@ -5,11 +5,25 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-
-
 export default function OpeningTimes(props) {
-
   const { openingTimes } = props;
+
+  if (!openingTimes || openingTimes.length === 0) {
+    // Handle the case where openingTimes is undefined or empty
+    return (
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h3" gutterBottom>
+            Öffnungszeiten
+          </Typography>
+          <p>No opening times available.</p>
+        </CardContent>
+        <CardActions>
+          <Button size="small" variant="contained">Termin buchen</Button>
+        </CardActions>
+      </Card>
+    );
+  }
 
   return (
     <Card sx={{ minWidth: 275 }}>
@@ -17,13 +31,16 @@ export default function OpeningTimes(props) {
         <Typography variant="h3" gutterBottom>
           Öffnungszeiten
         </Typography>
-        {openingTimes.map((dayData, index) => (
-          <li key={index}>
-            <strong>{dayData.day}:</strong><br />
-            Morning: {new Date(dayData['open-morning']).toLocaleTimeString()} - {new Date(dayData['closing-morning']).toLocaleTimeString()}<br />
-            Afternoon: {new Date(dayData['open-afternoon']).toLocaleTimeString()} - {new Date(dayData['closing-afternoon']).toLocaleTimeString()}<br />
-          </li>
-        ))}
+        <ul>
+          {openingTimes.map((dayData, index) => (
+            <li key={index}>
+              <strong>{dayData.day}:</strong><br />
+              Morning: {new Date(dayData['open-morning']).toLocaleTimeString('de-CH', {hour: '2-digit', minute: '2-digit'})} - {new Date(dayData['closing-morning']).toLocaleTimeString('de-CH', {hour: '2-digit', minute: '2-digit'})}<br />
+              Afternoon: {new Date(dayData['open-afternoon']).toLocaleTimeString('de-CH', {hour: '2-digit', minute: '2-digit'})} - {new Date(dayData['closing-afternoon']).toLocaleTimeString('de-CH', {hour: '2-digit', minute: '2-digit'})}<br />
+              Lunch Break: {dayData['has-lunch-break'] ? 'Yes' : 'No'}
+            </li>
+          ))}
+        </ul>
       </CardContent>
       <CardActions>
         <Button size="small" variant="contained">Termin buchen</Button>
