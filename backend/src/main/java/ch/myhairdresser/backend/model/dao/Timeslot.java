2a3e0e6173f5e3947d34c7a1ecb4242c50eb5d9e
iribzuehlke <irdin.ibisevic@gmail.com>
2023-10-10 12:14:44 +0200

    Timeslot wip

--- backend/src/main/java/ch/myhairdresser/backend/model/dao/Timeslot.java
+++ backend/src/main/java/ch/myhairdresser/backend/model/dao/Timeslot.java
@@ -1,12 +1,11 @@
 package ch.myhairdresser.backend.model.dao;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Time;
 
 @Entity
 @Getter
@@ -17,5 +16,11 @@ public class Timeslot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="open_morning")
+    private Time start;
+
+    @Column(name="closing_morning")
+    private Time end;
 
 }
--- testdata.sql
+++ testdata.sql
@@ -90,3 +90,6 @@ INSERT INTO image (link, hairsalon_id) VALUES
 		id, appointmentidentifier, firstname, lastname, mail, telephone, description, duration, price, date, hairsalon_id)
 VALUES (9, '53i105f5-aag8-61d2-cf65-9e8e2991gf65', 'David', 'Dream', 'david.dream@mail.ch', '079 824 67 45', 'Lorem ipsum solor dit amet',  '65M', 100, '2023-10-31', 1);
 
+INSERT INTO public.appointment(
+    id, appointmentidentifier, firstname, lastname, mail, telephone, description, duration, price, date, hairsalon_id)
+VALUES (10, '23j215g6-aag9-72e3-de76-7f9f3092gh76', 'Luis', 'Muriel', 'luis.muriel@mail.ch', '079 834 78 56', 'Lorem ipsum solor dit amet', '45M', 75, '2023-10-30', 1);
