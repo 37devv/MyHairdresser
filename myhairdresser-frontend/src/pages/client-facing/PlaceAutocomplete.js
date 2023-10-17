0af0978d8d4ed2a2283761f54f51f1922ac270df
iribzuehlke <irdin.ibisevic@gmail.com>
2023-10-18 00:41:22 +0200

    longi and latitude displaying

--- myhairdresser-frontend/src/pages/client-facing/PlaceAutocomplete.js
+++ myhairdresser-frontend/src/pages/client-facing/PlaceAutocomplete.js
@@ -1,15 +1,13 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { TextField, Autocomplete } from '@mui/material';
 import debounce from 'lodash/debounce';
 
 function PlaceAutocomplete() {
   const [options, setOptions] = useState([]);
-  
+  const [selectedPlace, setSelectedPlace] = useState(null);  // Store the selected place details
 
   const loadSuggestions = (value) => {
-    // Create a new session token. 
     const sessionToken = new window.google.maps.places.AutocompleteSessionToken();
-    
     const autocompleteService = new window.google.maps.places.AutocompleteService();
 
     autocompleteService.getPlacePredictions({
@@ -17,25 +15,53 @@ function PlaceAutocomplete() {
       sessionToken: sessionToken
     }, (predictions, status) => {
       if (status === window.google.maps.places.PlacesServiceStatus.OK && predictions) {
-        setOptions(predictions.map(prediction => prediction.description));
+        setOptions(predictions);
       }
     });
   }
 
-  // Debounce the loadSuggestions function so it waits 300ms after the last call to execute.
+  const fetchPlaceDetails = (placeId) => {
+    const placesService = new window.google.maps.places.PlacesService(document.createElement('div'));
+    placesService.getDetails({
+      placeId: placeId,
+      fields: ['geometry']
+    }, (place, status) => {
+      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+        setSelectedPlace(place);
+      }
+    });
+  }
+
   const debouncedLoadSuggestions = debounce(loadSuggestions, 300);
 
+  const latitude = selectedPlace?.geometry?.location?.lat();
+  const longitude = selectedPlace?.geometry?.location?.lng();
+
   return (
+    <>
     <Autocomplete
       style={{ width: 300 }}
       onInputChange={(event, value) => {
         debouncedLoadSuggestions(value);
       }}
+      onChange={(event, value) => {
+        if (value) {
+          fetchPlaceDetails(value.place_id);
+        } else {
+          setSelectedPlace(null);
+        }
+      }}
       options={options}
+      getOptionLabel={(option) => option.description}
       renderInput={(params) => (
         <TextField {...params} label="Place" fullWidth />
       )}
     />
+    {"lat"+ latitude}
+    <br></br>
+    {"long" + longitude}
+    </>
+    
   );
 }
 
