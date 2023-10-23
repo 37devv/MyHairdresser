de583ffbaa1be00b9bc7cbec8b70b362e47f8e00
iribzuehlke <irdin.ibisevic@gmail.com>
2023-10-23 19:31:59 +0200

    UI verschönerung

--- myhairdresser-frontend/src/pages/client-facing/LandingPage.js
+++ myhairdresser-frontend/src/pages/client-facing/LandingPage.js
@@ -4,35 +4,38 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import BookingBox from './BookingBox';
 import Pricing from './Pricing';
-
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 function LandingPage() {
-
-
     return (
         <React.Fragment>
-
-
-            <Container maxWidth="lg">
-                <Grid container spacing={10}>
+            <Container maxWidth="lg" sx={{marginTop: 5}}>
+                {/* Set the alignItems prop to 'stretch' for the Grid container */}
+                <Grid container spacing={10} alignItems="stretch">
                     <Grid item xs={4}>
-                        <Typography variant="h2">
-                            MyHairdresser
-                        </Typography>
-                        <Typography variant="h3">
-                            Buchungsplattform um Termine leichter zur vergeben für Coiffeure!
-                        </Typography>
+                        <Paper elevation={3} sx={{ height: '100%' }}>
+                            <Box sx={{ width: '100%', padding: 2 }}>
+                                <Typography variant="h2">
+                                    MyHairdresser
+                                </Typography>
+                                <br />
+                                <Typography variant="h3">
+                                    Buchungsplattform um Termine leichter zur vergeben für Coiffeure!
+                                </Typography>
+                            </Box>
+                        </Paper>
                     </Grid>
                     <Grid item xs={8}>
-
-                        <BookingBox />
-
+                        <Paper elevation={3} sx={{ height: '100%' }}>
+                            <BookingBox />
+                        </Paper>
                     </Grid>
                 </Grid>
-                
                 <Pricing />
             </Container>
         </React.Fragment>
     );
 }
+
 export default LandingPage;
